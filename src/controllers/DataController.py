b31a27b52434dff9ef85d182411d9cf172e1d2e7
mahmoudmoe84 <mahmoud.moawed@gmail.com>
2025-08-23 16:07:16 +0000

    added schema and prcoess controller

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -4,6 +4,8 @@ from models import ResponseSignal
 from .ProjectController import ProjectController
 import re
 import os
+
+
 class DataController(BaseController):
     
     
--- src/routes/data.py
+++ src/routes/data.py
@@ -6,6 +6,7 @@ from controllers import DataController, ProjectController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemas.data import ProcessRequest
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -42,3 +43,10 @@ async def upload_data(project_id:str,file: UploadFile,app_settings:Settings = De
         status_code = status.HTTP_200_OK,
         content = {"signal":ResponseSignal.FILE_UPLOADED_SUCCESSFULLY.value,
                    "file_id":file_id})
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, 
+                           process_request: ProcessRequest):
+    file_id = process_request.file_id
+    
+    return file_id
