d849466dc2ff69734d48099b4eb370af67edf7db
mahmoudmoe84 <mahmoud.moawed@gmail.com>
2025-08-30 17:31:21 +0000

    added chunks model

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,6 +9,8 @@ import logging
 from .schemas.data import ProcessRequest
 from controllers.ProcessController import ProcessController
 from models.ProjectModel import ProjectModel
+from models.ChunkModel import ChunkModel
+from models.db_schemes import DataChunk
 
 
 
@@ -56,13 +58,18 @@ async def upload_data(request: Request, project_id:str,file: UploadFile,app_sett
                    "project_id":str(project.id)
                    }
         )
-
 @data_router.post("/process/{project_id}")
-async def process_endpoint(project_id:str, 
+async def process_endpoint(request:Request, project_id:str, 
                            process_request: ProcessRequest):
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     overlap_size = process_request.overlap_size
+    
+    project_model = ProjectModel(db_client=request.app.db_client)
+    
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+    
+    
     process_controller = ProcessController(project_id=project_id) 
     
     file_content = process_controller.get_file_content(file_id=file_id)
@@ -78,4 +85,17 @@ async def process_endpoint(project_id:str,
             status_code = status.HTTP_400_BAD_REQUEST,
             content = {"signal":ResponseSignal.PROCESSING_FAILED.value}
         )
-    return file_chunks
+        
+    file_chunks_records = [
+        DataChunk(
+            chunk_text=chunk.page_content,
+            chunk_metadata=chunk.metadata,
+            chunk_order = i+1,
+            chunk_project_id=project.id
+        )
+        for i,chunk in enumerate(file_chunks)
+    ]
+    chunk_model = ChunkModel(db_client=request.app.db_client)
+    no_records = await chunk_model.insert_many_chunks(chunks=file_chunks_records)
+    return no_records
+                                                
